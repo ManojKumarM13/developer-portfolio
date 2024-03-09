ddf7cdd63921259d37fb17947a03c10a68307532
ManojKumarM13 <147139182+ManojKumarM13@users.noreply.github.com>
2024-03-09 23:11:03 +0530

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'manojkumarmarabattina@gmail.com',
+    phone: '+91 6301815560',
+    address: 'Chennai,TamilNadu, India -602105 ',
+    github: 'https://github.com/ManojKumarM13',
+    facebook: '',
+    linkedIn: 'www.linkedin.com/in/manoj-kumar-marabattina-12a32b238',
+    twitter: '',
+    stackOverflow: '',
+    devUsername: ""
 }
